e318865f6be1d5fff30569e116afcdeda092da16
Paweł Pacana <pawel.pacana@gmail.com>
2022-08-24 19:22:58 +0200

    Useless
    * settings are shadowed by bind mount of civ5save volume

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -e
 # Fetch the dir where this bash script is
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+sudo cp "$DIR/server/GraphicsSettingsDX9.ini" "$DIR/civ5save/GraphicsSettingsDX9.ini"
 sudo cp "$DIR/server/UserSettings.ini" "$DIR/civ5save/UserSettings.ini"
 
 # Build the container
