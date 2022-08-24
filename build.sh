35032651824fa99fe6518c6a65df1ed3e120dca0
Paweł Pacana <pawel.pacana@gmail.com>
2022-08-24 20:31:49 +0200

    Yet another configuration
    * this one enables is mostly default one but enables
    
      MessageLog = 1
      LoggingEnabled = 1

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Bail out if any command fails
 set -e
 
 # Fetch the dir where this bash script is
@@ -8,6 +7,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 sudo cp "$DIR/server/GraphicsSettingsDX9.ini" "$DIR/civ5save/GraphicsSettingsDX9.ini"
 sudo cp "$DIR/server/UserSettings.ini" "$DIR/civ5save/UserSettings.ini"
+sudo cp "$DIR/server/config.ini" "$DIR/civ5save/config.ini"
 
 # Build the container
 docker build -t civ5server "$DIR/server"
